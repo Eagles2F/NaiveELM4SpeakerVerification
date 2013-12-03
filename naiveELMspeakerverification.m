bc8d8ef971b65b3c0a102f49b73775ac3131c464
Eagles2F <thueeliyifan@gmail.com>
2013-12-03 15:22:15 +0800

    First Script create

---
+++ naiveELMspeakerverification.m
@@ -0,0 +1,6 @@
+%   Title: Naive ELM for speaker verification
+%   Author: Yifan Li
+%   Create Time: 12/3/2013
+%   Brief: 
+%           This is the script for the naive ELM speaker verification
+%           experiments.
